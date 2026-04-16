dfbf86cfed55e108ee602db3a59066102e616bd2
Shihyun Han <devowl99@gmail.com>
2026-04-16 21:39:23 +0900

    260416

--- src/javaps/INF0812.java
+++ src/javaps/INF0812.java
@@ -15,16 +15,16 @@ public class INF0812 {
     static int[] dy = {-1, 1, 0, 0};
 
     static class Tomato {
-        int x, y;
+        int x, y, day;
 
-        Tomato(int x, int y){
+        Tomato(int x, int y, int day){
             this.x = x;
             this.y = y;
+            this.day = day;
         }
     }
 
     static int tCount;
-    static int emp;
 
     public static void main(String[] args) throws IOException {
 
@@ -35,37 +35,31 @@ public class INF0812 {
         box = new int[N][M];
 
         tCount = 0;
-        emp = 0;
+
+        q = new ArrayDeque<>();
+
         for (int n=0; n<N; n++){
             st = new StringTokenizer(br.readLine());
 
             for (int m=0; m<M; m++){
                 int t = Integer.parseInt(st.nextToken());
-                if (t == 0) tCount++;
-                else if (t == -1) emp++;
+                if (t == 0) tCount++; // 안 익은 토마토 수 세기
+                if (t == 1) q.offer(new Tomato(m, n, 0));
                 box[n][m] = t;
             }
         }
 
-        if (tCount+emp == N*M) System.out.println(0);
+        if (tCount == 0) System.out.println(0); // 안 익은 토마토가 없는 경우
         else System.out.println(bfs());
     }
 
     static int bfs(){
         int date = 0;
 
-        q = new ArrayDeque<>();
-
-        for (int y=0; y<N; y++){
-            for (int x=0; x<M; x++){
-                if (box[y][x] == 1) q.offer(new Tomato(x, y));
-            }
-        }
-
         while (!q.isEmpty()){
             Tomato t = q.poll();
 
-            date = Math.max(date, box[t.y][t.x]);
+            date = t.day;
 
             for (int d=0; d<4; d++){
                 int nx = t.x + dx[d];
@@ -73,15 +67,15 @@ public class INF0812 {
 
                 // 1 : 익음, 0 : 익지 않음, -1 : 빈 칸
                 if (nx<0 || nx>=M || ny<0 || ny>=N) continue;
-                if (box[ny][nx] != 0) continue;
+                if (box[ny][nx] != 0) continue; // 안 익은 토마토 아닌 경우
 
-                box[ny][nx] = box[t.y][t.x] + 1;
+                box[ny][nx] = 1;
                 tCount--;
-                q.offer(new Tomato(nx, ny));
+                q.offer(new Tomato(nx, ny, t.day+1));
             }
         }
 
-        if (tCount == 0) return date-1;
+        if (tCount == 0) return date;
         else return -1;
     }
 }
