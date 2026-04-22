0b11e14ec8ae278dbc8e77a74b3e0e6477e3ca7c
Shihyun Han <devowl99@gmail.com>
2026-04-22 14:57:35 +0900

    260422

--- src/javaps/INF0905v2.java
+++ src/javaps/INF0905v2.java
@@ -79,10 +79,10 @@ public class INF0905v2 {
                 int nextCost = next.cost;
 
                 // dis에 저장된 최소 cost <= 현재 노드까지 cost + 현재->다음 노드 cost
-                if (dis[nextNode] <= dis[curNode] + nextCost) continue;
-
-                dis[nextNode] = dis[curNode] + nextCost;
-                pQ.offer(new Node(nextNode, dis[nextNode]));
+                if (dis[nextNode] > dis[curNode] + nextCost) {
+                    dis[nextNode] = dis[curNode] + nextCost;
+                    pQ.offer(new Node(nextNode, dis[nextNode]));
+                }
             }
         }
     }
