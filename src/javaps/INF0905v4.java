04272a1126ee2d381ce2d95471e93fe4bf4b116c
Shihyun Han <devowl99@gmail.com>
2026-04-27 09:28:52 +0900

    260427

--- src/javaps/INF0905v4.java
+++ src/javaps/INF0905v4.java
@@ -9,12 +9,73 @@ public class INF0905v4 {
 
     static int N, M;
 
+    static class Node implements Comparable<Node> {
+        int to;
+        int cost;
+
+        Node (int to, int cost){
+            this.to = to;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o){
+            return this.cost - o.cost;
+        }
+    }
+
+    static final int INF = Integer.MAX_VALUE;
+    static int[] dis;
+    static List<Node>[] graph;
+
     public static void main(String[] args) throws IOException {
 
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+        graph = new ArrayList[N+1];
+        for (int n=1; n<=N; n++){
+            graph[n] = new ArrayList<>();
+        }
 
+        for (int m=0; m<M; m++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            graph[from].add(new Node(to, cost));
+        }
+
+        dij();
+
+        for (int i=2; i<=N; i++){
+            if (dis[i] != INF) System.out.println(i+" : "+dis[i]);
+            else System.out.println(i+" : impossible");
+        }
+    }
+
+    static void dij() {
+        PriorityQueue<Node> pQ = new PriorityQueue<>();
+        dis = new int[N+1];
+        Arrays.fill(dis, INF);
+
+        int start = 1;
+        pQ.offer(new Node(start, 0));
+        dis[start] = 0;
+
+        while (!pQ.isEmpty()){
+            Node cur = pQ.poll();
+
+            if (dis[cur.to] < cur.cost) continue;
+
+            for (Node next: graph[cur.to]){
+                if (dis[next.to] > dis[cur.to] + next.cost){
+                    dis[next.to] = dis[cur.to] + next.cost;
+                    pQ.offer(new Node(next.to, dis[next.to]));
+                }
+            }
+        }
     }
 }
