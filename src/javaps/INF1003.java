17a50fcf813622d66759e69689ca43b5887a6c34
Shihyun Han <devowl99@gmail.com>
2026-04-30 17:42:02 +0900

    260430

---
+++ src/javaps/INF1003.java
@@ -0,0 +1,22 @@
+package javaps;
+
+import java.io.*;
+import java.util.*;
+
+public class INF1003 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+
+        st = new StringTokenizer(br.readLine());
+
+        dp = new int[N+1];
+
+
+    }
+}
