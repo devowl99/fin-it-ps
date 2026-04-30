7ccf634be746c8d3dcd44a6cddf7183243d613bd
Shihyun Han <devowl99@gmail.com>
2026-04-30 16:36:24 +0900

    260430

--- src/javaps/INF0908v3.java
+++ src/javaps/INF0908v3.java
@@ -3,29 +3,28 @@ package javaps;
 import java.io.*;
 import java.util.*;
 
-public class INF0908v2 {
+public class INF0908v3 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
     static int V, E;
 
-    static class Node implements Comparable<Node>{
+    static class Edge implements Comparable<Edge>{
         int to;
         int cost;
 
-        Node(int to, int cost){
+        Edge(int to, int cost) {
             this.to = to;
             this.cost = cost;
         }
 
         @Override
-        public int compareTo(Node o){
+        public int compareTo(Edge o){
             return Integer.compare(this.cost, o.cost);
         }
     }
 
-    static List<Node>[] graph;
-    static boolean[] visited;
+    static List<Edge>[] graph;
 
     public static void main(String[] args) throws IOException {
 
@@ -33,45 +32,50 @@ public class INF0908v2 {
         V = Integer.parseInt(st.nextToken());
         E = Integer.parseInt(st.nextToken());
 
-        graph = new ArrayList[V+1];
-        for (int i=0; i<=V; i++){
-            graph[i] = new ArrayList<>();
+        graph = new ArrayList[V + 1];
+        for (int v = 0; v <= V; v++) {
+            graph[v] = new ArrayList<>();
         }
 
-        for (int e=0; e<E; e++){
+        for (int e = 0; e < E; e++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
 
-            graph[from].add(new Node(to, cost));
-            graph[to].add(new Node(from, cost));
+            graph[from].add(new Edge(to, cost));
+            graph[to].add(new Edge(from, cost));
         }
 
-        visited = new boolean[V+1];
-        PriorityQueue<Node> pQ = new PriorityQueue<>();
+        int answer = prim();
 
-        pQ.offer(new Node(1, 0));
+        System.out.println(answer);
+    }
 
+    static int prim() {
+        PriorityQueue<Edge> pQ = new PriorityQueue<>();
+        boolean[] visited = new boolean[V+1];
         int count = 0;
         int totalCost = 0;
-        while (!pQ.isEmpty()){
-            Node cur = pQ.poll();
+
+        pQ.offer(new Edge(1, 0));
+        while (!pQ.isEmpty()) {
+            Edge cur = pQ.poll();
 
             if (visited[cur.to]) continue;
+
             visited[cur.to] = true;
-            totalCost += cur.cost;
             count++;
+            totalCost += cur.cost;
 
-            if (count == V) break;
+            if (count == V) break; // 최적화 용도
 
-            for (Node next: graph[cur.to]){
-                if (visited[next.to]) continue;
-
+            for (Edge next: graph[cur.to]) {
+                if (visited[next.to]) continue; // 이미 방문한 노드로의 간선은 저장 X (최적화 용도)
                 pQ.offer(next);
             }
         }
 
-        System.out.println(totalCost);
+        return totalCost;
     }
 }
