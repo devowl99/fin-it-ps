de31a05001cbf3dca9a53a51ca722fc04f8649c7
Shihyun Han <devowl99@gmail.com>
2026-05-10 23:42:47 +0900

    260510

---
+++ src/javaps/INF1004v3.java
@@ -0,0 +1,31 @@
+package javaps;
+
+import java.io.*;
+import java.util.*;
+
+public class INF1004v3 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static int N;
+    static class Brick implements {
+        int s;
+        int h;
+        int w;
+
+        Brick(int s, int h, int w) {
+            this.s = s;
+            this.h = h;
+            this.w = w;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+        for (int i=0; i<N; i++){
+
+        }
+
+    }
+}
