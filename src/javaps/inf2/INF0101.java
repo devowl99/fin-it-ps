284a60400684f641b6276f47977fc7f797a5caa9
Shihyun Han <devowl99@gmail.com>
2026-09-05 16:17:28 +0900

    260905

--- src/javaps/inf2/INF0101.java
+++ src/javaps/inf2/INF0101.java
@@ -1,4 +1,25 @@
 package javaps.inf2;
 
+import java.io.*;
+
 public class INF0101 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String str = br.readLine().toUpperCase();
+        char c = br.readLine().toUpperCase().charAt(0);
+
+        System.out.println(solution(str, c));
+    }
+
+    static int solution(String str, char c){
+        int answer = 0;
+
+        for (char x: str.toCharArray()) {
+            if (x == c) answer++;
+        }
+
+        return answer;
+    }
 }
