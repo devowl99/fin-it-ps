df462c8e5f3c9bfc4f2e0049ff33d378cd8716c2
Shihyun Han <devowl99@gmail.com>
2026-09-06 02:45:44 +0900

    260906

---
+++ src/javaps/inf2/INF0106.java
@@ -0,0 +1,21 @@
+package javaps.inf2;
+
+import java.io.*;
+
+public class INF0106 {
+
+    static String solution(String str) {
+        String answer = "";
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        solution(str);
+    }
+}
