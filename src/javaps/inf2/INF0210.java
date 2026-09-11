4dc61a73db417e99d05de990dcc075b744c49174
Shihyun Han <devowl99@gmail.com>
2026-09-11 12:50:15 +0900

    260911

---
+++ src/javaps/inf2/INF0210.java
@@ -0,0 +1,13 @@
+package javaps.inf2;
+
+import java.io.*;
+import java.util.*;
+
+public class INF0210 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
