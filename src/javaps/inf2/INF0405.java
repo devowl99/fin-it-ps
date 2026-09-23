d2d4d4b5bc8e71c2b7edb8a45d96312772cdbf79
Shihyun Han <devowl99@gmail.com>
2026-09-23 09:10:21 +0900

    260923

--- src/javaps/inf2/INF0405.java
+++ src/javaps/inf2/INF0405.java
@@ -1,4 +1,34 @@
 package javaps.inf2;
 
+import java.io.*;
+import java.util.*;
+
 public class INF0405 {
+
+    static int solution(int N, int K, int[] arr) {
+        Set<Integer> ts = new TreeSet<>(Collections.reverseOrder());
+        for (int x: arr) {
+            ts.add(x);
+        }
+
+        List<Integer> lst = new ArrayList<>(ts);
+
+        return lst.get(K-1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int n=0; n<N; n++) {
+            arr[n] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(N, K, arr));
+    }
 }
